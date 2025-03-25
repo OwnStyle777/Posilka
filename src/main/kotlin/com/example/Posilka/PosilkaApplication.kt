77c41aacc36d65f96165012e936d1f4b635b9f37
OwnStyle777 <OwnStyleXD@gmail.com>
2025-03-25 19:23:43 +0100

    init commit

---
+++ src/main/kotlin/com/example/Posilka/PosilkaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Posilka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PosilkaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PosilkaApplication>(*args)
+}
