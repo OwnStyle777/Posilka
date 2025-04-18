f8b5651b848416ed286ad1469e84762925eecd4a
OwnStyle777 <OwnStyleXD@gmail.com>
2025-04-18 10:00:03 +0200

    remove promise

--- frontend/Posilka/api/TrainingService.ts
+++ frontend/Posilka/api/TrainingService.ts
@@ -4,26 +4,22 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:8080/api/trainings';
 
 
-export const getTrainingById = async (id: number): Promise<Training> => {
-  const response = await axios.get(`${API_BASE_URL}/${id}`);
-  return response.data;
-};
-
-export const getAllTrainings = async (): Promise<Training[]> => {
-  const response = await axios.get(API_BASE_URL);
-  return response.data;
-};
-
-export const createTraining = async (training: Training): Promise<Training> => {
-  const response = await axios.post(API_BASE_URL, training);
-  return response.data;
-};
-
-export const updateTraining = async (id: number, training: Training): Promise<Training> => {
-  const response = await axios.put(`${API_BASE_URL}/${id}`, training);
-  return response.data;
-};
-
-export const deleteTraining = async (id: number): Promise<void> => {
-  await axios.delete(`${API_BASE_URL}/${id}`);
-};
+export const getTrainingById = (id: number): Promise<Training> => {
+    return axios.get(`${API_BASE_URL}/${id}`).then(response => response.data);
+  };
+  
+  export const getAllTrainings = (): Promise<Training[]> => {
+    return axios.get(API_BASE_URL).then(response => response.data);
+  };
+  
+  export const createTraining = (training: Training): Promise<Training> => {
+    return axios.post(API_BASE_URL, training).then(response => response.data);
+  };
+  
+  export const updateTraining = (id: number, training: Training): Promise<Training> => {
+    return axios.put(`${API_BASE_URL}/${id}`, training).then(response => response.data);
+  };
+  
+  export const deleteTraining = (id: number): Promise<void> => {
+    return axios.delete(`${API_BASE_URL}/${id}`).then(() => {});
+  };
